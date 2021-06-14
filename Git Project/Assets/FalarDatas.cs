ea7666884f7d925b8d700182bd2de549975b6eaa
Matheus-Rag <matheus@midnightcreators.com>
2021-06-14 20:52:28 -0300

    Primeiro envio
    Script cabulozo

---
+++ Git Project/Assets/FalarDatas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FalarDatas : MonoBehaviour
+{
+    public Transform[] g;
+    void Start()
+    {
+        foreach(Transform t in g)
+        {
+            Debug.Log( t.position );
+        }
+    }
+
+}
